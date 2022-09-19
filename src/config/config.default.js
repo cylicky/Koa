fa3d2be2df991c434da3c14018c32333ca7559ff
Cyao <2906882615@qq.com>
2022-09-19 18:34:51 +0800

    2022.9.19

--- src/config/config.default.js
+++ src/config/config.default.js
@@ -1,7 +1,11 @@
 const dotenv = require('dotenv');
 const path = require('path');
 // 需要配置一下本地 文件所在位置
-dotenv.config({ path: path.resolve(__dirname, '../.env') });
+//dotenv.config({ path: path.resolve(__dirname, '../.env') , allowEmptyValues: true});
+dotenv.config({
+    path: path.resolve(__dirname, '../.env'),
+    allowEmptyValues: true
+});
 
 //导出配置文件
 module.exports = process.env;
